8f3db1c5cdf353a79732864aba11d3a5c7384d43
hamzaelboukri <hamzaelboukri01@gmail.com>
2024-12-27 09:04:23 +0100

    add ajouter

--- class.php
+++ class.php
@@ -1,32 +1,82 @@
 <?php
-$NeoBank = new PDO("mysql:host=localhost;dbname=neobank", "root", "");
-$NeoBank->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $NEobank = new PDO('mysql:host=localhost;dbname=neobank', 'root', '');
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    echo "Failed to connect to the database: " . $e->getMessage();
+}
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+if (isset($_POST['accountNumber']) && isset($_POST['ownerName']) && isset($_POST['accountType'])) {
     $accountNumber = $_POST['accountNumber'];
     $ownerName = $_POST['ownerName'];
     $accountType = $_POST['accountType'];
-    
-    try {
-        switch($accountType) {
-            case 'savings':
-                $account = new SavingsAccount($NeoBank, $accountNumber, $ownerName, 0.0, $_POST['interestRate']);
-                break;
-            case 'current':
-                $account = new CurrentAccount($NeoBank, $accountNumber, $ownerName, 0.0, $_POST['overdraftLimit']);
-                break;
-            case 'business':
-                $account = new BusinessAccount($NeoBank, $accountNumber, $ownerName, 0.0, $_POST['task-transaction']);
-                break;
-        }
-        
-        $account->createAccount();
-        header("Location: index.php");
-        
-    } catch(Exception $e) {
-        echo $e->getMessage();
-    }
+    $interestRate = $_POST['interestRate'];
+    $overdraftLimit = $_POST['overdraftLimit'];
+    $taskTransaction = $_POST['taskTransaction'];
+
 }
 ?>
+
+
+<?php
+
+class acount {
+    public $accountNumber;
+    public $ownerName;
+    public $accountType;
+    public $interestRate;
+    public $overdraftLimit;
+    public $taskTransaction;
+
+    public function __construct($accountNumber, $ownerName, $accountType, $interestRate, $overdraftLimit, $taskTransaction) {
+        $this->accountNumber = $accountNumber;
+        $this->ownerName = $ownerName;
+        $this->accountType = $accountType;
+        $this->interestRate = $interestRate;
+        $this->overdraftLimit = $overdraftLimit;
+        $this->taskTransaction = $taskTransaction;
+    }
+
+    public function save() {
+        global $NEobank;
+        $query = $NEobank->prepare("INSERT INTO account (accountNumber,
+         ownerName,
+          accountType,
+           Rate,
+            overdraftLimit, 
+            taskTransaction) VALUES (:accountNumber, 
+            :ownerName,
+             :accountType, 
+             :Rate, 
+             :overdraftLimit,
+              :task)");
+        $query->bindParam(':accountNumber', $this->accountNumber);
+        $query->bindParam(':ownerName', $this->ownerName);
+        $query->bindParam(':accountType', $this->accountType);
+        $query->bindParam(':interestRate', $this->interestRate);
+        $query->bindParam(':overdraftLimit', $this->overdraftLimit);
+        $query->bindParam(':task', $this->taskTransaction);
+        $query->execute();
+    }
+
+    public function getAccount() {
+        global $NEobank;
+        $query = $NEobank->prepare("SELECT * FROM account");
+        $query->execute();
+        return $query->fetchAll();
+    }
+
+}
+
+
+
+
+
+
+
+?>
+
+
 
 
